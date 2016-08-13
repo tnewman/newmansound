9f9736b88078995631caa7167a982a10d7480530
Tom Newman <tnewman1@ltu.edu>
2016-08-13 13:02:44 -0400

    Documented fixtures

--- server/tests/fixtures.py
+++ server/tests/fixtures.py
@@ -7,6 +7,8 @@ from newmansound.model import Base
 
 @pytest.fixture(scope='session')
 def engine():
+    """Creates an in-memory SQLAlchemy database engine that will be created once and reused for all tests."""
+
     in_memory_engine = create_engine('sqlite:///:memory:')
 
     Base.metadata.create_all(bind=in_memory_engine)
@@ -16,6 +18,8 @@ def engine():
 
 @pytest.fixture(scope='function')
 def session(request, engine):
+    """Creates a new SQLAlchemy Session for each test that will be rolled back after each test execution."""
+
     session_class = sessionmaker(bind=engine)
     session = session_class()
 
